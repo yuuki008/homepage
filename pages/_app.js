35337e24566c0168e34fc2791cec66988ef4e559
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-11-08 23:11:05 +0900

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../lib/theme'
+import Layout from '../components/layouts/main'
+import { AnimatePresence } from 'framer-motion'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
