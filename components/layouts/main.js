a047a8ba296a8c5749a32df669164de9cf226955
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-11-10 22:41:03 +0900

    favicon

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -19,7 +19,7 @@ const Main = ({ children, router, setLocale, locale }) => {
         <meta name="author" content="Takuya Matsuyama" />
         <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/images/nomu.png" type="image/x-icon" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@craftzdog" />
