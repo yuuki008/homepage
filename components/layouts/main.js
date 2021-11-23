4ea61720e207bac0b6a6eb005c9e2311ee380067
yuuki008 <s22ah261@seinan-gakuin.jp>
2021-11-23 16:59:11 +0900

    fix

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,7 +15,7 @@ const Main = ({ children, router, setLocale, locale }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
+        <meta name="description" content="Nomu's homepage" />
         <meta name="author" content="Yuuki Noumura" />
         <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
